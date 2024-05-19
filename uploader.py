ff4d124476711c3449effb9c0a7692c5bafa1914
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-05-19 16:41:19 -0700

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -4,6 +4,7 @@ import re
 class Uploader:
   def __init__(self):
     pass
-  def upload_string(self, content):
+  @staticmethod
+  def upload_string(content):
     content = re.sub(r'\W+', '', content)
     subprocess.check_output(f"""echo "{content}" | curl -T - https://api.pastes.dev/post""", shell=True).decode("utf-8")
